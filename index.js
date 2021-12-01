5e8490a10f0b27a5059359fbeb0a5581900abe75
dan-sada <daniyal.javed@teamsada.com>
2021-11-30 21:51:09 -0500

    add test

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const BillingReporter = require('./billing_reporter');
 const SlackWebhooker = require('./slack_webhooker');
-const DataStudio = require('./slack_webhooker');
 
 const PROJECT_ID = process.env.PROJECT_ID;
 const BILLING_ACCOUNT_ID = process.env.BILLING_ACCOUNT_ID.replace(/-/g, '_');
@@ -15,35 +14,10 @@ function main() {
     
     );
     const slackWebhooker = new SlackWebhooker(WEBHOOK_URL);
-    const announce       = "this is a test";
 
-    return billingReporter.query().then(
-        result [slackWebhooker.post(result), announce
-        ]).then(console.log).catch(console.error); 
+    return [billingReporter.query().then(
+        result => slackWebhooker.post(result)
+    ).then(console.log).catch(console.error), 'test123']; 
 }
 
-// function datastudio() {
-//     const billingReporter = new BillingReporter(
-//         PROJECT_ID,
-//         BILLING_ACCOUNT_ID,
-//         BILLING_DATASET,
-    
-//     );
-//     const slackWebhooker = new SlackWebhooker(WEBHOOK_URL);
-
-//     return billingReporter.query().then(
-//         slackWebhooker.post('testing 123')
-//     ).then(console.log).catch(console.error);
-// }
-
-// function datastudio() {
-//     const slackWebhooker = new SlackWebhooker(WEBHOOK_URL);
-//     const announce       = "this is a test";
-
-//     return announce(
-//         announce => slackWebhooker.post(announce)
-//     )
-// }
-
 module.exports.main = main;
-// module.exports.datastudio = datastudio;
